0ed72423345f7b6e07cdb2c10fc5ff1e81808cb7
Hugo Locurcio <hugo.locurcio@hugo.pro>
2021-02-04 01:40:31 +0100

    Continue work on character controller, add smoothing-addon

--- actors/player.gd
+++ actors/player.gd
@@ -1,16 +1,53 @@
-extends KinematicBody
+extends Spatial
+
+const MOUSE_SENSITIVITY = 0.002
+const RUN_SPEED = 2
+const JUMP_VELOCITY = 0.24
+const GRAVITY = 32
+
+onready var kinematic_body := $KinematicBody as KinematicBody
+onready var pivot := $Smoothing/Pivot as Spatial
+onready var raycast := $KinematicBody/RayCast as RayCast
+
+var velocity := Vector3()
 
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	move_and_collide(Vector3(0, -delta, 0))
+	# Apply Doom-style friction.
+	velocity.x *= 0.925
+	velocity.y *= 0.996
+	velocity.z *= 0.925
+
+	# Apply movement keys.
+	velocity += (
+			pivot.transform.basis.x * (Input.get_action_strength("move_right") - Input.get_action_strength("move_left")) * RUN_SPEED +
+			pivot.transform.basis.z * (Input.get_action_strength("move_backward") - Input.get_action_strength("move_forward")) * RUN_SPEED
+	)
+
+	# Apply jumping.
+	if Input.is_action_just_pressed("jump") and raycast.is_colliding():
+		# Movement is is applied only once.
+		# Compensate for `move_and_slide()`'s automatic delta calculation.
+		velocity.y += JUMP_VELOCITY / delta
+
+	# Apply gravity.
+	if not raycast.is_colliding():
+		velocity.y -= GRAVITY * delta
+
+	kinematic_body.move_and_slide(velocity)
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			pivot.rotate(Vector3.UP, -event.relative.x * MOUSE_SENSITIVITY)
+
+	if event.is_action_pressed("toggle_mouse_capture"):
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
